9b1af9f389ce09d8f2420a291a1a5a880bfa1a54
Peter M <petem14@gmail.com>
2018-11-08 13:07:42 -0500

    completed w/o sort

--- main.js
+++ main.js
@@ -81,10 +81,3 @@ function appendCounts() {
         document.getElementById("wordsGraph").appendChild(div);
     }
 }
-
-
-function compare(a,b) {
-    if (a.value < b.value) return -1;
-    if (a.value < b.value) return 1;
-    return 0;
-}
